6c873a5b948ebfbd89a9dbba6fb273ab23f90fe7
bryanoke25 <bryan.oke25@gmail.com>
2024-08-22 18:21:59 +0100

    shoot_the_alien.py

--- shoot_the_alien.py
+++ shoot_the_alien.py
@@ -9,15 +9,15 @@ message = ""
 alien = Actor("alien")
 
 def draw():
-    screen.clear()
-    screen.fill(color = (128, 0, 0))
+  screen.clear()
+  screen.fill(color = (128, 0, 0))
 
-    alien.draw()
-    screen.draw.text(message, center = (400, 10), fontsize= 30)
+  alien.draw()
+  screen.draw.text(message, center = (400, 10), fontsize= 30)
 
 def place_alien():
-    alien.x = randint(50, WIDTH-50)
-    alien.y = randint(50, HEIGHT-50)
+  alien.x = randint(50, WIDTH-50)
+  alien.y = randint(50, HEIGHT-50)
 
 def on_mouse_down(pos):
   #print("Hello World")
@@ -27,6 +27,8 @@ def on_mouse_down(pos):
     place_alien()
   else:
     message = "You missed"
+    place_alien()
+
 
 # This method needs to be called to start processing
 place_alien()
